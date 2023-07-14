a72cf9ec1eeb0f9679a0dbaa897073ddfd72f9ab
HODOR72 <72arkhipov@mail.ru>
2023-07-14 14:13:08 +0500

    fix bugs

--- src/redux/thunks/messages.ts
+++ src/redux/thunks/messages.ts
@@ -1,17 +1,15 @@
 import { dispatch } from '../store';
-import { MessagesManager } from '../../@types/messages';
+import { MessagesList } from '../../@types/messages';
 import ApiClients from '../../utils/axios';
 import { hasError, addMessagesSuccess } from '../slices/messages';
 
 const { axiosBase } = ApiClients;
 
 export function createMessagesThunk(params: any) {
+  console.log(params);
   return async () => {
     try {
-      const response: { data: { data: MessagesManager } } = await axiosBase.get(
-        `sendMessages`,
-        params
-      );
+      const response: { data: MessagesList } = await axiosBase.get(`sendMessages`, params);
       dispatch(addMessagesSuccess(response.data.data));
       return true;
     } catch (error) {

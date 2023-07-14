628e4c5dce5830bd6e8b02a92697b0efbd3c659e
HODOR72 <72arkhipov@mail.ru>
2023-07-14 13:35:11 +0500

    delete post response

--- src/contexts/JWTContext.tsx
+++ src/contexts/JWTContext.tsx
@@ -83,12 +83,7 @@ function AuthProvider({ children }: AuthProviderProps) {
   const login = async (login: string, password: string) => {
     console.log(login, password);
 
-    const response = await axiosBase.post('/login', {
-      login,
-      password,
-    });
-
-    console.log(response)
+    const response = await axiosBase.get(`/login?login=${login}&password=${password}`);
 
     Cookies.set('logged', 'True');
 

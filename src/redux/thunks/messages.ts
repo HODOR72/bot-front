008e8f393ca2b94deb1f7f8f9d7b042a4f857d7a
HODOR72 <72arkhipov@mail.ru>
2023-07-14 14:21:38 +0500

    fix

--- src/redux/thunks/messages.ts
+++ src/redux/thunks/messages.ts
@@ -9,7 +9,10 @@ export function createMessagesThunk(params: any) {
   console.log(params);
   return async () => {
     try {
-      const response: { data: MessagesList } = await axiosBase.get(`sendMessages`, params);
+      const response: { data: MessagesList } = await axiosBase.get(
+        `sendMessages?users=${params.users}&message=${params.message}`
+      );
+
       dispatch(addMessagesSuccess(response.data.data));
       return true;
     } catch (error) {
